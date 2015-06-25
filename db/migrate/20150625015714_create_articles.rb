c836d744657f67d5b54b0b5adf0cfcf5b0e3cd48
Dan Comand <dcomand@gmail.com>
2015-06-24 21:59:13 -0400

    Created the article model.

---
+++ db/migrate/20150625015714_create_articles.rb
@@ -0,0 +1,12 @@
+class CreateArticles < ActiveRecord::Migration
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.string :image
+      t.string :summary
+      t.string :url
+
+      t.timestamps null: false
+    end
+  end
+end
